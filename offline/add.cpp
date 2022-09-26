078f4abefd694d16e2d214fd0108d53e88e8da46
Rasmus Nuko <rnukoj@gmail.com>
2022-09-26 17:05:00 +0200

    formatting

--- offline/add.cpp
+++ offline/add.cpp
@@ -15,25 +15,29 @@ Source add(vector<Source> sources){
   result.reserve(num_channels);
 
   for (int channel = 0; channel < num_channels; ++channel){
+    // New vector for new channel
+    vector<double> new_channel;
     // Reserve memory for result vec<vec>
-    result[channel].reserve(num_sources * num_samples)
+    new_channel.reserve(num_sources * num_samples)
 
     // Concat sources' channels in result vector
     for (int source = 0; source < num_sources; ++source){
-      result[channel].insert(std::end(result[channel]),
-                             std::begin(multi_channels[source][channel]),
-                             std::end(multi_channels[source][channel]))
+      vector<double> source_channel = multi_channels[source][channel];
+      new_channel.insert(end(new_channel), begin(source_channel), end(source_channel));
     }
 
     // Add samples together corresponding to:
     // result_{i} = sum_{s in sources} multi_channels[s][c][i], for c in channel, for i in samples
     for (int sample = num_samples; sample < result.size(); ++sample){
-      result[channel][samples % num_samples] += result[channel][sample];
+      new_channel[samples % num_samples] += new_channel[sample];
     }
-  }
 
-  // Truncate result
-  result.resize(num_samples);
+    // Truncate result to only fit num samples
+    new_channel.resize(num_samples);
+
+    // Add new channel to result
+    result.push_back(new_channel);
+  }
 
   return result;
 }
