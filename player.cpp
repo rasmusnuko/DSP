17512f947260bda74749718e3ec1c16b29ebee92
Rasmus Nuko <rnukoj@gmail.com>
2022-09-29 19:11:23 +0200

    Started

---
+++ player.cpp
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <math.h>
+#include "portaudio.h"
+
+#include "source.h"
+
+#define SAMPLE_RATE   (44100)
+#define FRAMES_PER_BUFFER  (64)
+
+#ifndef M_PI
+#define M_PI  (3.14159265)
+#endif
+
+
+class Player
+{
+public:
+    Player(Source source) :
+    {
+    }
+
+    bool open(PaDeviceIndex index, Source source)
+    {
+        PaStreamParameters outputParameters;
+
+        outputParameters.device = index;
+        if (outputParameters.device == paNoDevice) {
+            return false;
+        }
+
+        const PaDeviceInfo* pInfo = Pa_GetDeviceInfo(index);
+        if (pInfo != 0)
+        {
+            printf("Output device name: '%s'\r", pInfo->name);
+        }
+
+        outputParameters.channelCount = source.num_channels;       /* stereo output */
+        outputParameters.sampleFormat = source.bit_depth; /* 32 bit floating point output */
+        outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+        outputParameters.hostApiSpecificStreamInfo = NULL;
+
+        PaError err = Pa_OpenStream(
+            &stream,
+            NULL, /* no input */
+            &outputParameters,
+            SAMPLE_RATE,
+            paFramesPerBufferUnspecified,
+            paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+            &Player::paCallback,
+            this            /* Using 'this' for userData so we can cast to Sine* in paCallback method */
+            );
+
+        if (err != paNoError)
+        {
+            /* Failed to open stream to device !!! */
+            return false;
+        }
+
+        err = Pa_SetStreamFinishedCallback( stream, &Player::paStreamFinished );
+
+        if (err != paNoError)
+        {
+            Pa_CloseStream( stream );
+            stream = 0;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    bool close()
+    {
+        if (stream == 0)
+            return false;
+
+        PaError err = Pa_CloseStream( stream );
+        stream = 0;
+
+        return (err == paNoError);
+    }
+
+
+    bool start()
+    {
+        if (stream == 0)
+            return false;
+
+        PaError err = Pa_StartStream( stream );
+
+        return (err == paNoError);
+    }
+
+    bool stop()
+    {
+        if (stream == 0)
+            return false;
+
+        PaError err = Pa_StopStream( stream );
+
+        return (err == paNoError);
+    }
+
+private:
+    /* The instance callback, where we have access to every method/variable in object of class Sine */
+    int paCallbackMethod(const void *inputBuffer, void *outputBuffer,
+        unsigned long framesPerBuffer,
+        const PaStreamCallbackTimeInfo* timeInfo,
+        PaStreamCallbackFlags statusFlags)
+    {
+        float *out = (float*)outputBuffer;
+        unsigned long i;
+
+        (void) timeInfo; /* Prevent unused variable warnings. */
+        (void) statusFlags;
+        (void) inputBuffer;
+
+        for( i=0; i<framesPerBuffer; i++ )
+        {
+            *out++ = source[left_phase];  /* left */
+            *out++ = source[right_phase];  /* right */
+            left_phase++;
+            right_phase++;
+        }
+
+        return paContinue;
+
+    }
+
+    /* This routine will be called by the PortAudio engine when audio is needed.
+    ** It may called at interrupt level on some machines so don't do anything
+    ** that could mess up the system like calling malloc() or free().
+    */
+    static int paCallback( const void *inputBuffer, void *outputBuffer,
+        unsigned long framesPerBuffer,
+        const PaStreamCallbackTimeInfo* timeInfo,
+        PaStreamCallbackFlags statusFlags,
+        void *userData )
+    {
+        /* Here we cast userData to Sine* type so we can call the instance method paCallbackMethod, we can do that since
+           we called Pa_OpenStream with 'this' for userData */
+        return ((Player*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
+            framesPerBuffer,
+            timeInfo,
+            statusFlags);
+    }
+
+
+    void paStreamFinishedMethod()
+    {
+        printf( "Stream Completed: %s\n", message );
+    }
+
+    /*
+     * This routine is called by portaudio when playback is done.
+     */
+    static void paStreamFinished(void* userData)
+    {
+        return ((Player*)userData)->paStreamFinishedMethod();
+    }
+
+    Source source;
+    int left_phase;
+    int right_phase;
+};
+
+class ScopedPaHandler
+{
+public:
+    ScopedPaHandler()
+        : _result(Pa_Initialize())
+    {
+    }
+    ~ScopedPaHandler()
+    {
+        if (_result == paNoError)
+        {
+            Pa_Terminate();
+        }
+    }
+
+    PaError result() const { return _result; }
+
+private:
+    PaError _result;
+};
+
+
+/*******************************************************************/
+int main(void);
+int main(void)
+{
+    Player player;
+
+    printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
+
+    ScopedPaHandler paInit;
+    if( paInit.result() != paNoError ) goto error;
+
+    if (player.open(Pa_GetDefaultOutputDevice(), source))
+    {
+        if (sine.start())
+        {
+            printf("Play for %d seconds.\n", NUM_SECONDS );
+            Pa_Sleep( NUM_SECONDS * 1000 );
+
+            sine.stop();
+        }
+
+        sine.close();
+    }
+
+    printf("Test finished.\n");
+    return paNoError;
+
+error:
+    fprintf( stderr, "An error occurred while using the portaudio stream\n" );
+    fprintf( stderr, "Error number: %d\n", paInit.result() );
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( paInit.result() ) );
+    return 1;
+}
