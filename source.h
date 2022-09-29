506937c78ddc6fa3badc7bc230d753c6f622510d
Rasmus Nuko <rnukoj@gmail.com>
2022-09-29 19:11:04 +0200

    Changed types

--- source.h
+++ source.h
@@ -3,41 +3,87 @@
 
 #include <vector>
 
-typedef struct Source{
-  int num_channels;
-  int sample_rate;
-  int bit_depth;
-  std::vector<std::vector<double> > channels;
+class Source{
+  public:
+    int num_channels;
+    int sample_rate;
+    int bit_depth;
+    std::vector<std::vector<float> > channels;
 
-  // Constructor
-  Source(){
-    // Default values
-    num_channels = 2;
-    sample_rate = 44100;
-    bit_depth = 16;
-
-    // Reserve memory for channels
-    channels.reserve(num_channels);
-
-    // Populate channels with double vectors
-    for (int c = 0; c < num_channels; ++c){
-      channels.push_back(std::vector<double>());
+    /*
+     * Update channels to correspond to set amount of channels
+     */
+    void update_channels(){
+      this->channels = std::vector<std::vector<float> >();
+      this->channels.reserve(num_channels);
+      for (int c = 0; c < this->num_channels; ++c){
+        this->channels.push_back(std::vector<float>());
+      }
     }
-  }
-} Source;
+
+    /*
+     * Constructor
+     */
+    Source(int num_channels = 2, int sample_rate = 44100, int bit_depth = 16){
+      // Default values
+      this->num_channels = num_channels;
+      this->sample_rate  = sample_rate;
+      this->bit_depth    = bit_depth;
+      this->channels     = std::vector<std::vector<float> >();
+
+      this->update_channels();
+    }
+
+    /*
+     * Copies a Source
+     */
+    Source copy(){
+      Source result;
+      result.num_channels = this->num_channels;
+      result.sample_rate  = this->sample_rate;
+      result.bit_depth    = this->bit_depth;
+      result.channels     = this->channels;
+    
+      return result;
+    }
+    
+};
 
 /*
- * Copies a Source. Is called as:
- * Source new_copy = original_source.copy();
+ * Generate test Source of a sine wave
  */
-inline Source copy_source(Source source){
-  Source result;
-  result.num_channels = source.num_channels;
-  result.sample_rate = source.sample_rate;
-  result.bit_depth = source.bit_depth;
-  result.channels = source.channels;
+Source sine_test(int num_channels = 2, float frequency = 220, int duration = 2){
+    // Make new source and reserve memory
+    Source sine = Source(num_channels);
+    printf("Sine source generated\n");
+    sine.num_channels = num_channels;
+    printf("Source num channels: %i\n", sine.num_channels);
+    printf("Source allocated Cs: %lu\n", sine.channels.size());
+    printf("Source sample rate:  %i\n", sine.sample_rate);
+    printf("Source bit depth:    %i\n", sine.num_channels);
+    int total_samples = duration * sine.sample_rate;
 
-  return result;
+    // Reserving memory in all channels for total amount of samples
+    for (int c = 0; c < sine.num_channels; ++c){
+      sine.channels[c].reserve(total_samples);
+    }
+    printf("Memory allocated for %i samples pr channel", total_samples);
+    
+
+    // Sine wave
+    int max_amplitude = pow(2, sine.bit_depth - 1) - 1;
+    for(int i = 0; i < total_samples; i++){
+        float amplitude = ((float)i / sine.sample_rate) * max_amplitude;
+        float value = sin((2 * 3.14159 * i * frequency) / sine.sample_rate);
+        // Write to all channels
+        for (int c = 0; c < sine.num_channels; ++c){
+          sine.channels[c].push_back(amplitude * value / 2);
+        }
+    }
+
+    printf("Generated sine source");
+
+    return sine;
 }
 
 #endif
