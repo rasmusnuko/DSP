b95d7545c16f193422572016e08de378a499e41a
Rasmus Nuko <rnukoj@gmail.com>
2022-10-13 13:46:41 +0200

    tested

--- src/test/wavetable_test.cpp
+++ src/test/wavetable_test.cpp
@@ -1,26 +1,28 @@
 #include "../wavetable.cpp"
 
+string prefix = "output/wavetable/";
+
 void sinewave_test(){
   Source sine = ::sinewave(220, 2);
-  string filepath = "output/sinewave_wavetable_test.wav";
+  string filepath = prefix + "sinewave_wavetable_test.wav";
   ::render_wav(sine, filepath);
 }
 
 void sawtooth_test(){
   Source sawtooth = ::sawtooth(220, 2);
-  string filepath = "output/sawtooth_wavetable_test.wav";
+  string filepath = prefix + "sawtooth_wavetable_test.wav";
   ::render_wav(sawtooth, filepath);
 }
 
 void triangle_test(){
   Source triangle = ::triangle(220, 2);
-  string filepath = "output/triangle_wavetable_test.wav";
+  string filepath = prefix + "triangle_wavetable_test.wav";
   ::render_wav(triangle, filepath);
 }
 
 void square_test(){
   Source square = ::square(220, 2);
-  string filepath = "output/square_wavetable_test.wav";
+  string filepath = prefix + "square_wavetable_test.wav";
   ::render_wav(square, filepath);
 }
 
